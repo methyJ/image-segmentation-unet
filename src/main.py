044580ad3cfdb8a49601da695548f3908a3bd8fb
Metin Ahmet Özmen <ozmenmetinahmet@gmail.com>
2026-01-22 19:28:35 +0300

    model

--- src/main.py
+++ src/main.py
@@ -7,19 +7,15 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Dropout 
 from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import concatenate
-from test_utils import summary, comparator
-from preprocess import preprocess, process_path
+from preprocess_helper import preprocess, process_path
 import os
-import pandas as pd
-import imageio.v2 as imageio
-import matplotlib.pyplot as plt
-from unet import conv_block, upsampling_block
+from unet import conv_block, upsampling_block, unet_model
 
 IMAGE_DIR = "./data/CameraRGB"
 MASK_DIR  = "./data/CameraMask"
 
-# ---- file pairing (same filename) ----
-files = sorted(os.listdir(IMAGE_DIR))  # sorted: image-mask eşleşmesi garanti olsun
+
+files = sorted(os.listdir(IMAGE_DIR))  # image-mask matching
 image_list = [os.path.join(IMAGE_DIR, f) for f in files]
 mask_list  = [os.path.join(MASK_DIR,  f) for f in files]
 
@@ -32,4 +28,19 @@ dataset = tf.data.Dataset.from_tensor_slices(
 )
 image_ds = dataset.map(process_path)
 processed_image_ds = image_ds.map(preprocess)
+
+img_height = 96
+img_width = 128
+num_channels = 3
+
+unet = unet_model((img_height, img_width, num_channels))
+unet.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+EPOCHS = 5
+VAL_SUBSPLITS = 5
+BUFFER_SIZE = 500
+BATCH_SIZE = 32
+train_dataset = processed_image_ds.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+model_history = unet.fit(train_dataset, epochs=EPOCHS)
 
--- src/unet.py
+++ src/unet.py
@@ -1,4 +1,5 @@
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, concatenate
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Conv2DTranspose, concatenate, Input
 
 def conv_block(inputs=None, n_filters=32, dropout_prob=0, max_pooling=True):
     conv = Conv2D(
@@ -48,3 +49,33 @@ def upsampling_block(expansive_input, contractive_input, n_filters=32):
                  kernel_initializer="he_normal")(conv)
     
     return conv
+
+def unet_model(input_size=(96, 128, 3), n_filters=32, n_classes=23):
+
+    inputs = Input(input_size)
+
+
+    cblock1 = conv_block(inputs, n_filters)
+    cblock2 = conv_block(cblock1[0], n_filters * 2)
+    cblock3 = conv_block(cblock2[0], n_filters * 4)
+    cblock4 = conv_block(cblock3[0], n_filters * 8, dropout_prob=0.3)  # dropout 0.3
+    cblock5 = conv_block(cblock4[0], n_filters * 16, dropout_prob=0.3, max_pooling=False)  # no max pool
+
+    ublock6 = upsampling_block(cblock5[0], cblock4[1], n_filters * 8)
+    ublock7 = upsampling_block(ublock6,    cblock3[1], n_filters * 4)
+    ublock8 = upsampling_block(ublock7,    cblock2[1], n_filters * 2)
+    ublock9 = upsampling_block(ublock8,    cblock1[1], n_filters)
+
+
+    conv9 = Conv2D(n_filters,
+                   3,
+                   activation='relu',
+                   padding='same',
+                   kernel_initializer='he_normal')(ublock9)
+
+    conv10 = Conv2D(n_classes, 1, padding='same')(conv9)
+
+    
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
+
+    return model
